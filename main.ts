d7735403e28effa45998c6c7564ed2f742c32edc
byronlove111 <malik.chan94420@gmail.com>
2025-12-04 20:59:46 +0100

    Remove unused files related to the Anthropic integration, including main application logic, model entity, repository interface, and message service.

---
+++ main.ts
@@ -0,0 +1,54 @@
+import Anthropic from "@anthropic-ai/sdk";
+import "dotenv/config";
+import readline from "readline/promises";
+
+type Message = {
+  role: "user" | "assistant";
+  content: string;
+};
+
+async function main() {
+  const anthropic = new Anthropic();
+  const messages: Message[] = [];
+  anthropic.apiKey = process.env.ANTHROPIC_API_KEY || null;
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  console.log("Chat with AI. Type 'exit' or CTRL + C to end the conversation.");
+  while (true) {
+    let lastMessage: string;
+
+    lastMessage = await rl.question("");
+    if (lastMessage === "debug") {
+      console.log(messages);
+      continue;
+    }
+    if (lastMessage === "exit") break;
+    messages.push({
+      role: "user",
+      content: lastMessage,
+    });
+
+    const message = await anthropic.messages.create({
+      max_tokens: 1000,
+      messages: messages,
+      model: "claude-haiku-4-5-20251001",
+    });
+
+    if (message.content[0].type === "text") {
+      messages.push({
+        role: "assistant",
+        content: message.content[0].text,
+      });
+      console.log("\n\nAI : ", message.content[0].text, "\n\n");
+    }
+  }
+  rl.close();
+}
+
+main().catch((error) => {
+  console.log(error);
+});
--- src/interfaces/model.repository.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IModelRepository {
-  sendMessage(textContent: string): Promise<string | undefined>;
-}
